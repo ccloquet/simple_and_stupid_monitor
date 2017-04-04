faa8ba592e071efe84845fab798861a201313a1b
Christophe Cloquet <ccloquet@users.noreply.github.com>
2017-04-04 14:57:42 +0200

    Update

--- collectdata.sh
+++ collectdata.sh
@@ -1,6 +1,7 @@
+find ./*.csv -mtime +7 -exec rm {} \;
+find ./*.txt -mtime +7 -exec rm {} \;
 export fname=$(date +"%Y%m%d")
-vmstat -t 5 10 | tail -n5 > $fname.txt
 while true; do
+    vmstat -t 5 10 | tail -n5 >> $fname.txt
    ./parseout.sh $fname
-   vmstat -t 5 10 | tail -n5 >> $fname.txt
 done
