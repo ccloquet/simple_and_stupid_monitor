0c3fbb438c760113cd769a283d4e1af99b6f9c3b
Christophe Cloquet <ccloquet@users.noreply.github.com>
2017-04-04 14:36:52 +0200

    Create parseout.sh

---
+++ parseout.sh
@@ -0,0 +1,6 @@
+cat $1.txt | sed 's/ \+/,/g' | awk -F',' ' 20 {print $19" "$20 $0}'  > $1.csv
+for i in {2..18..1}
+    do
+        cat $1.csv | awk -v col="$i" -F',' ' 20 {print $1","$col}'  > $1_$i.csv
+    done
+
